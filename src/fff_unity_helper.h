98c642b97a94565937185f59c49939d94a9a302a
Cormac Cannon <cormac.cannon@neuromoddevices.com>
2018-03-28 14:35:50 +0100

    Added some convenient macros for function call verification.
    A number of these use the __VA_ARGS__ iteration macros in vaiter64.h
    
    Extensions:
    TEST_ASSERT_CALLED(FN, ...) -- extended to optionally verify function call
    arguments. Still works without arguments.
    
    Additions:
    TEST_ASSERT_NTH_CALL(FN, CALL_IDX, ...) As TEST_ASSERT_CALLED, but for
    the case where the function has been called multiple times. The
    CALL_IDX argument specifies the index of the call to verify.
    
    TEST_ASSERT_LAST_CALL(FN, ...) As TEST_ASSERT_CALLED, but doesn't assume
    function was only called once.
    
    TEST_ASSERT_CALLS(FN, N_CALLS) As existing TEST_ASSERT_CALLED_TIMES, but
    parameter order consistent with other variadic additions.
    
    SET_RETURN_VAL_SEQ(FN, ...) Convenient wrapper for SET_RETURN_SEQ, but
    allows the sequence to be defined inline in the macro call, saving some
    boilerplate.
    
    SET_RETURN_VAL(FN, VAL) Sets return value -- insulates end user from
    FFF structure setup and consistent with SET_RETURN_VAL_SEQ etc.

--- src/fff_unity_helper.h
+++ src/fff_unity_helper.h
@@ -15,7 +15,7 @@
 /*
     Fail if the function was not called exactly once.
 */
-#define TEST_ASSERT_CALLED(function_) TEST_ASSERT_CALLED_TIMES(1, function_)
+/* #define TEST_ASSERT_CALLED(function_) TEST_ASSERT_CALLED_TIMES(1, function_) */
 
 /*
     Fail if the function was called 1 or more times.
@@ -29,5 +29,90 @@
     TEST_ASSERT_EQUAL_PTR_MESSAGE((void *) function_,                       \
         fff.call_history[order_],                                           \
         "Function " #function_ " not called in order " #order_ )
+
+/*
+  Fail if the function was not called with the given parameter values
+ */
+#include "vaiter64.h"
+#define _FFF_NAME(FN) FN##_fake
+#define _FFF_CALLS(FN) _FFF_NAME(FN).call_count
+#define _FFF_LAST_VAL(FN, IDX) _FFF_NAME(FN).arg##IDX##_val
+#define _FFF_NTH_VAL(FN, CALL_IDX, ARG_IDX) _FFF_NAME(FN).arg##ARG_IDX##_history[CALL_IDX-1]
+
+
+#define _FFF_VERIFY_ARG(FN, VAL, ARG_IDX) TEST_ASSERT_EQUAL(VAL, _FFF_LAST_VAL(FN, ARG_IDX));
+/**
+ * Verifies that the named function was called once and only once, and optionally verifies its arguments
+ * @param FN The mocked function name
+ * @param ... Expected values of the function arguments
+ */
+#define TEST_ASSERT_CALLED(FN, ...)                  \
+  TEST_ASSERT_CALLED_TIMES(1, FN);                   \
+  VA_FIX_IDX_EACH(_FFF_VERIFY_ARG, FN, __VA_ARGS__)
+
+#define _FFF_VERIFY_HISTORICAL_ARG_(FN, CALL_IDX, VAL, ARG_IDX) TEST_ASSERT_EQUAL(VAL, _FFF_NTH_VAL(FN, CALL_IDX, ARG_IDX));
+/**
+ * Verifies that the named function at least CALL_IDX times, and optionally verifies the arguments for that call
+ * @param FN The mocked function name
+ * @param CALL_IDX The (1-based) call index
+ * @param ... Expected values of the function arguments
+ */
+#define TEST_ASSERT_NTH_CALL(FN, CALL_IDX, ...)                         \
+  TEST_ASSERT_TRUE(CALL_IDX > 0);                                       \
+  TEST_ASSERT_TRUE(_FFF_CALLS(FN) >= CALL_IDX);                         \
+  VA_2FIX_IDX_EACH(_FFF_VERIFY_HISTORICAL_ARG_, FN, CALL_IDX, __VA_ARGS__)
+
+/**
+ * Verifies that the named function was called at least once, and optionally verifies the arguments for the final call
+ * @param FN The mocked function name
+ * @param ... Expected values of the function arguments
+ */
+#define TEST_ASSERT_LAST_CALL(FN, ...) TEST_ASSERT_NTH_CALL(FN, _FFF_CALLS(FN), __VA_ARGS__)
+
+/**
+ * Verify that the named function was called COUNT times
+ * @param FN The mocked function name
+ * @param COUNT The expected number of calls
+ * This duplicates the functionality of the existing TEST_ASSERT_CALLED_TIMES, but reverses the parameter order
+ * for API consistency with the new variadic macro assertions.
+ */
+#define TEST_ASSERT_CALLS(FN, COUNT) TEST_ASSERT_EQUAL(COUNT, _FFF_CALLS(FN))
+
+// Should these additions get pushed upstream to the fff project rather than ceedling/fff
+#define SET_RETURN_VAL(FN, VAL) FN##_fake.return_val = VAL
+
+/**
+ * Set a sequence of consecutive return values for a given mocked function
+ * @param FN The function name
+ * @param ... The sequence of return values
+ * This is a convenient wrapper for existing FFF functionality:
+ * @code
+ * SET_RETURN_VAL_SEQ(SomeModule_doSomething, 1, 2, 3, 4, 5);
+ * @endcode
+ * ... expands to ...
+ * @code
+ * {
+ *   typeof(SomeModule_doSomething.return_val) return_seq[] = {1, 2, 3, 4, 5};
+ *   SET_RETURN_SEQ(SomeModule_doSomething, return_seq, 5);
+ * }
+ * @endcode
+ */
+#define SET_RETURN_VAL_SEQ(FN, ...)                                     \
+  typeof(FN##_fake.return_val) FN##_retseq[] = {__VA_ARGS__};           \
+  SET_RETURN_SEQ(FN, FN##_retseq, sizeof(FN##_retseq)/sizeof(FN##_retseq[0]))
+
+/**
+ * Repeat one or more operations a given number of times
+ * @param REP_COUNT The number of repetitions
+ * @param ... The action(s) to repeat
+ *
+ * (This is handy when writing tests, but probably doesn't belong here.)
+ */
+#define REPEAT(REP_COUNT, ...) {                \
+    uint16_t repetitions = REP_COUNT;           \
+    while(repetitions--) {                      \
+      __VA_ARGS__;                              \
+    }                                           \
+  }
 
 #endif
